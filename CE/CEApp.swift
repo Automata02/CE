edac57c8f9dafe3e888a2e3b4dc6999d54deb023
Automata02 <roberts.kursitis@gmail.com>
2025-02-18 08:47:24 +0200

    Initial Commit

---
+++ CE/CEApp.swift
@@ -0,0 +1,17 @@
+//
+//  CEApp.swift
+//  CE
+//
+//  Created by automata on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CETests/CETests.swift
@@ -0,0 +1,17 @@
+//
+//  CETests.swift
+//  CETests
+//
+//  Created by automata on 18/02/2025.
+//
+
+import Testing
+@testable import CE
+
+struct CETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
